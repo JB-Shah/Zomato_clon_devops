25040dc7038e5a49ee76e6836ad14613d20bec6d
JB-Shah <jenils2003@gmail.com>
2024-07-28 18:10:28 +0530

    Modified install_tools.sh

--- Jenkins_setup/install_tools.sh
+++ Jenkins_setup/install_tools.sh
@@ -3,11 +3,8 @@
 
 # --- Installing java ---
 sudo apt update -y
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 java -version
-openjdk version "17.0.8" 2023-07-18
-OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
-OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
 
 # --- Installing Jenkins ---
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
@@ -37,7 +34,7 @@ docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install trivy -y
 
 
